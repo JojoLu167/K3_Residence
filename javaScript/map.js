d803e1b13dc8e6b717960b64f868d119ecd377b1
JojoLu167 <JohannaKoe@gmx.de>
2026-03-17 01:15:59 +0100

    map with right pins

--- javaScript/map.js
+++ javaScript/map.js
@@ -1,127 +1,79 @@
-const map = L.map('map').setView([49.9928, 8.2315], 16);
+// K3 coordinates
+const k3 = [49.989023, 8.225469]
 
-L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-{ maxZoom: 19 }
-).addTo(map);
+// Create map
+const map = L.map('map').setView(k3, 17)
 
+// Tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; OpenStreetMap'
+}).addTo(map)
 
-const markers = {};
+// MARKERS
 
+const markers = {
+k3: L.marker([49.989023, 8.225469]).addTo(map)
+.bindPopup("K3"),
 
-/* Wohnheim */
+rewe: L.marker([49.99717,8.23097]).addTo(map)
+.bindPopup("Rewe"),
 
-markers.k3 = L.marker([49.9928, 8.2315])
-.addTo(map)
-.bindPopup("Wohnheim K3");
+kaufland: L.marker([49.966955,8.229197]).addTo(map)
+.bindPopup("Kaufland"),
 
+aldi: L.marker([49.976431,8.235278]).addTo(map)
+.bindPopup("Aldi"),
 
-/* Supermärkte */
+lidl: L.marker([49.968123,8.238088]).addTo(map)
+.bindPopup("Lidl"),
 
-markers.rewe = L.marker([49.9938, 8.2336])
-.addTo(map)
-.bindPopup(
-'<b>Rewe</b><br><a href="https://maps.app.goo.gl/ntU4sbNSgzqDGLgY9" target="_blank">Navigation</a>'
-);
+mueller: L.marker([49.967566,8.228019]).addTo(map)
+.bindPopup("Müller"),
 
-markers.kaufland = L.marker([49.9896, 8.2334])
-.addTo(map)
-.bindPopup(
-'<b>Kaufland</b><br><a href="https://maps.app.goo.gl/4GW1Nb7KvPjhub4S8" target="_blank">Navigation</a>'
-);
+dm: L.marker([50.000819,8.265538]).addTo(map)
+.bindPopup("DM"),
 
-markers.aldi = L.marker([49.9910, 8.2351])
-.addTo(map)
-.bindPopup(
-'<b>Aldi</b><br><a href="https://maps.app.goo.gl/BMJULPtabRRKrw1u5" target="_blank">Navigation</a>'
-);
+apo: L.marker([49.981899,8.244306]).addTo(map)
+.bindPopup("Sonnen Apotheke"),
 
-markers.lidl = L.marker([49.9934, 8.2359])
-.addTo(map)
-.bindPopup(
-'<b>Lidl</b><br><a href="https://maps.app.goo.gl/JeB9m2sFwfDjzw4Y9" target="_blank">Navigation</a>'
-);
+arena: L.marker([49.989408,8.223943]).addTo(map)
+.bindPopup(`
+🚊 Tram Stop: Jakob-Heinz-Straße / Arena<br>
+51, 53, 59
+`),
 
+campusbruecke: L.marker([49.990060,8.228261]).addTo(map)
+.bindPopup(`
+🚌 Bus Stop: Hochschule Mainz / Campusbrücke<br>
+74, 93, E
+`),
 
-/* Drogerie */
+kisselberg: L.marker([49.992112,8.224806]).addTo(map)
+.bindPopup(`
+🚌🚊 Kisselberg<br>
+Bus: 6, 54, 55, 56, 58, 68, 74, 76, 78, 91, 93, 630<br>
+Tram: 51, 53, 59
+`),
 
-markers.mueller = L.marker([49.9921, 8.2339])
-.addTo(map)
-.bindPopup('<b>Müller</b><br><a href="https://maps.app.goo.gl/Vq3vQME3mYdN821H9" target="_blank">Navigation</a>');
+sw: L.marker([49.992726,8.233475]).addTo(map)
+.bindPopup("Studierendenwerk"),
 
-markers.dm = L.marker([49.9924, 8.2332])
-.addTo(map)
-.bindPopup('<b>DM</b><br><a href="https://maps.app.goo.gl/HVDbV9bvBaMuUfPJ9" target="_blank">Navigation</a>');
+zm: L.marker([49.991801,8.234076]).addTo(map)
+.bindPopup("Zentralmensa")
 
+}
 
-/* Apotheke */
+// Focus function
+function focusMarker(name){
 
-markers.apo = L.marker([49.9933, 8.2342])
-.addTo(map)
-.bindPopup('<b>Sonnen-Apotheke</b><br><a href="https://maps.app.goo.gl/gMguMHt1U1276bnaA" target="_blank">Navigation</a>');
-
-
-/* Haltestellen */
-
-markers.arena = L.marker([49.9941, 8.2299])
-.addTo(map)
-.bindPopup("Tram: Bretzenheim J.-H.-Str / Arena");
-
-markers.campusbruecke = L.marker([49.9913, 8.2267])
-.addTo(map)
-.bindPopup("Bus: Campusbrücke");
-
-markers.kisselberg = L.marker([49.9879, 8.2314])
-.addTo(map)
-.bindPopup("Bus & Tram: Kisselberg");
-
-
-/* Studierendenwerk */
-
-markers.sw = L.marker([49.9893, 8.2362])
-.addTo(map)
-.bindPopup(
-'<b>Studierendenwerk Mainz</b><br><a href="https://www.studierendenwerk-mainz.de/" target="_blank">Website</a>'
-);
-
-
-/* Marker fokussieren */
-
-function focusMarker(id){
-
-const marker = markers[id];
+const marker = markers[name]
 
 if(marker){
 
-map.setView(marker.getLatLng(),17);
+map.setView(marker.getLatLng(),17)
 
-marker.openPopup();
-
-}
+marker.openPopup()
 
 }
 
-
-const groceries = L.layerGroup([
-markers.rewe,
-markers.kaufland,
-markers.aldi,
-markers.lidl
-]);
-
-const transport = L.layerGroup([
-markers.arena,
-markers.campusbruecke,
-markers.kisselberg
-]);
-
-L.control.layers(null,{
-"Lebensmittel":groceries,
-"Transport":transport
-}).addTo(map);
-
-const busIcon = L.icon({
-iconUrl:'icons/bus.png',
-iconSize:[28,28]
-});
-L.marker([lat,lng],{icon:busIcon})
+}
